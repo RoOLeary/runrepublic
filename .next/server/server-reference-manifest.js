154e3e804821de15047edbe123d64f3858ee3e29
Ronan <roleary81@gmail.com>
2024-01-15 18:11:12 +0100

    modify taxonomies

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"jREhJLqAAmXgGEEHkxsLHOO7otmboJNPbC3uF1aYUEE=\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"TWmKihRtk+pKF3yNzXDUtqmiTkH4UTVMYDaE8dz07/8=\"\n}"
--- src/components/Sections/HeaderFilter.tsx
+++ src/components/Sections/HeaderFilter.tsx
@@ -13,7 +13,7 @@ export interface HeaderFilterProps {
 }
 
 const HeaderFilter: FC<HeaderFilterProps> = ({
-  tabs = ["All items", "Startups", "Fitness", "Design"],
+  tabs = ["All items", "Startups", "SEO", "Design"],
   heading = "🎈 Latest Articles",
 }) => {
   const [tabActive, setTabActive] = useState<string>(tabs[0]);
