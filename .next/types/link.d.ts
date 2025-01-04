39002030e685292443913301ec9873a8a85a1d7c
Ronan O'Leary <roleary81@gmail.com>
2025-01-04 14:03:15 +0100

    again

--- .next/types/link.d.ts
+++ .next/types/link.d.ts
@@ -31,36 +31,36 @@ declare namespace __next_route_internal_types__ {
   type StaticRoutes = 
     | `/api/hello`
     | `/home-2`
-    | `/home-3`
     | `/home-4`
     | `/`
+    | `/home-3`
     | `/search-2`
     | `/search`
     | `/about`
     | `/contact`
     | `/forgot-pass`
+    | `/signup`
     | `/login`
     | `/subscription`
-    | `/signup`
-    | `/dashboard/edit-profile`
     | `/dashboard/posts`
-    | `/dashboard/subscription`
-    | `/dashboard`
     | `/dashboard/billing-address`
+    | `/dashboard`
+    | `/dashboard/subscription`
     | `/dashboard/submit-post`
+    | `/dashboard/edit-profile`
   type DynamicRoutes<T extends string = string> = 
+    | `/archive-3/${OptionalCatchAllSlug<T>}`
     | `/archive-2/${OptionalCatchAllSlug<T>}`
     | `/archive/${OptionalCatchAllSlug<T>}`
-    | `/archive-3/${OptionalCatchAllSlug<T>}`
     | `/author/${OptionalCatchAllSlug<T>}`
-    | `/single-audio/${OptionalCatchAllSlug<T>}`
     | `/single-2/${OptionalCatchAllSlug<T>}`
-    | `/single/${OptionalCatchAllSlug<T>}`
-    | `/single-gallery/${OptionalCatchAllSlug<T>}`
-    | `/single-video/${OptionalCatchAllSlug<T>}`
-    | `/single-4/${OptionalCatchAllSlug<T>}`
-    | `/single-5/${OptionalCatchAllSlug<T>}`
     | `/single-3/${OptionalCatchAllSlug<T>}`
+    | `/single-video/${OptionalCatchAllSlug<T>}`
+    | `/single/${OptionalCatchAllSlug<T>}`
+    | `/single-4/${OptionalCatchAllSlug<T>}`
+    | `/single-audio/${OptionalCatchAllSlug<T>}`
+    | `/single-5/${OptionalCatchAllSlug<T>}`
+    | `/single-gallery/${OptionalCatchAllSlug<T>}`
 
   type RouteImpl<T> = 
     | StaticRoutes
